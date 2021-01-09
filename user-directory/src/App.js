6e453ebb241fe1356f265c6c6c9068bc2bbd2dad
Ben Ivey <benjaminivey00@gmail.com>
2021-01-09 10:23:51 -0700

    started sortby functionality

--- user-directory/src/App.js
+++ user-directory/src/App.js
@@ -1,12 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
+import {data} from './data'
 import './App.css';
+import Body from './components/Body'
+import SortBy from './components/SortBy';
+
+
+
+
 
 function App() {
-  return (
-    <div className="App">
-    <h1>hello </h1>
-    </div>
-  );
+ 
+ const [ inputValue, setInputValue ] = useState('');
+ const [dataValue, setDataValue] = useState(data)
+ const [sortValue, setSortValue] = useState('')
+
+ const handleInputChange = e => {
+   setDataValue(data)
+   setInputValue(e.target.value)
+ };
+ 
+ const handleClick = e => {
+  e.preventDefault();
+  if(inputValue) {
+    setDataValue(dataValue.filter(person => {
+        return person.name.toLowerCase().includes(inputValue.toLowerCase().trim())
+    }))
+  setInputValue('');
+  }
+};
+
+
+return (
+  <>
+    <Heading title="Employee Directory"/>
+    <main>
+      <SearchBar inputValue={inputValue} handleInputChange={handleInputChange} handleClick={handleClick} />
+      <SortBy sortCategories={sortCategories} sortValue={sortValue} handleSortValueChange={handleSortValueChange}/>
+      {dataValue.length !== 0 ? <Table data={dataValue}/> : <h2 style={{textAlign: 'center', color:"#e54"}}>No employee found with that name.</h2> }    
+    </main>
+  </>
+);
 }
+  // return (
+  //   <div className="App">
+  //   <Body />
+  // </div>
+  // );
+
 
 export default App;
---
+++ user-directory/src/components/SortBy.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+function SortBy({ sortCategories, sortValue, handleSortValueChange }) {
+	return (
+		<section className='sort'>
+			<form className='sortForm'>
+				<label htmlFor='sortBy'>Sort By: </label>
+				<select name='sortBy' value={sortValue} onChange={handleSortValueChange} id='sortBy'>
+					<option value='' disabled hidden>
+						Choose One
+					</option>
+					{sortCategories.map(category => <option value={category}>{category}</option>)}
+				</select>
+			</form>
+		</section>
+	);
+}
+
+export default SortBy;
